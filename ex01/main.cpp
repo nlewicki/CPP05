23ea44e0601da7f7b3770807725267ee892d53a1
Nico <nlewicki@student.42heilbronn.de>
2025-06-03 13:24:17 +0200

    added some extra test cases

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 12:09:51 by nlewicki          #+#    #+#             */
-/*   Updated: 2025/03/11 12:10:09 by nlewicki         ###   ########.fr       */
+/*   Updated: 2025/06/03 13:22:48 by nlewicki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,16 @@ int main(void)
 	Bureaucrat b1("Lazy John", 150);
 	Bureaucrat b2("Exemplary Billy", 1);
 	Bureaucrat b3("Average Joe", 75);
-	Form f1("Form1", 1, 1);
+	Bureaucrat b4("Overachiever Alice", 3);
+	Form f1("Form1", 2, 1);
 	Form f2("Form2", 150, 150);
 	Form f3("Form3", 75, 75);
 
 	std::cout << b1 << std::endl;
 	std::cout << b2 << std::endl;
 	std::cout << b3 << std::endl;
+	std::cout << b4 << std::endl;
+	b4.signForm(f1);
 	b1.signForm(f1);
 	b2.signForm(f2);
 	b3.signForm(f3);
